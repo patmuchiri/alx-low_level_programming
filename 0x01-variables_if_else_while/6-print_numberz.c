30ed8b0ed6188e90ed67b3dfb15fe5ffd8524da6
patrickmuchiri <pmuchirig@gmail.com>
2023-08-11 17:39:56 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,18 +5,16 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
-	putchar('\n');
+	int c;
 
+	c = 0;
+	while
+		(c < 10)
+		{
+		putchar(c + '0')
+		c++;
+		}
+	putchar('\n');
 	return (0);
 }
 
