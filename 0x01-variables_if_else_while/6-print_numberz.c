4b5444a944d0ed1802a22643c84881a0c2c840ab
patrickmuchiri <pmuchirig@gmail.com>
2023-08-11 17:32:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+}
+
