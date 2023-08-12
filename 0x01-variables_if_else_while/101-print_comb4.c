ade8e8755ce6f4522b5fcdb13f0d89f0fe39d4ba
patrickmuchiri <pmuchirig@gmail.com>
2023-08-12 10:08:05 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,7 +20,7 @@ int main(void)
 					putchar (i);
 					putchar (j);
 					putchar (k);
-					if (!(i == '7' && j == '8' && tens == '9'))
+					if (!(i == '7' && j == '8' && k == '9'))
 					{
 						putchar (',');
 						putchar (' ');
