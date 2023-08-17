83bcbfd44c3b586563c3c09d39608ce2028cfcb4
patrickmuchiri <pmuchirig@gmail.com>
2023-08-17 15:18:56 -0700

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers from 0 to 9
+ * apart from 2 and 4
+ * num - function identifier
+ */
+
+void print_most_numbers(void)
+
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		if (num != '2' && num != '4')
+		{
+			_putchar (num);
+		}
+	}
+	_putchar ('\n');
+}
