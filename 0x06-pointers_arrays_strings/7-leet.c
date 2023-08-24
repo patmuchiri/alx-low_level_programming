9d67c52d7f6d51f24009022a199e0435649c9672
patrickmuchiri <pmuchirig@gmail.com>
2023-08-24 03:31:15 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,33 +9,26 @@
 
 char *leet(char *str)
 {
-	char leet_map[] = "AEOTL";
-	char leet_replacements[] = "43071";
+	char leet_map[] = "AEOTLaeot1";
+	char leet_replacements[] = "4307143071";
 
 	int i = 0;
 	int j = 0;
 
 	while (str[i] != '\0')
 	{
-		bool is_replacement = false;
-
-		for (j = 0; leet_map[j] != '\0'; j++)
+		j = 0;
+		while (leet_map[j] != '\0')
 		{
-			if (str[i] == leet_map[j] || str[i] == leet_map[j] + 32)
+			if (str[i] == leet_map[j])
 			{
 				str[i] = leet_replacements[j];
-				is_replacement = true;
 				break;
 			}
+			j++;
 		}
 
-		i += is_replacement ? 1 : 0;
-
-		if (!is_replacement)
-
-		{
-			i++;
-		}
+		i++;
 	}
 
 	return (str);
