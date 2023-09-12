e1d8c0103c4a5410a59029ed02e4abd68dd2adf2
patrickmuchiri <pmuchirig@gmail.com>
2023-09-12 10:22:01 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - provides for name and age of dog.
+ * @name: first parameter
+ * @age: second parameter
+ * @owner: third parameter
+ */
 
 struct dog
 {
