355cfd861b3711792c2294ae84222f387ac57924
patrickmuchiri <pmuchirig@gmail.com>
2023-09-14 13:57:45 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,11 +15,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
