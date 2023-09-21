e0949c8a980390bfb99001d6fe357f83d33235bd
patrickmuchiri <pmuchirig@gmail.com>
2023-09-21 13:00:35 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,6 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-
-	if (h == NULL)
-	{
-		printf("[%d], %s\n", h->len, h->str);
-
-		return (0);
-
-	}
-
 	while (h != NULL)
 	{
 		if (h->str == NULL)
