b987595c3aceb7c935a57fce5cee3995d1c0707c
patrickmuchiri <pmuchirig@gmail.com>
2023-09-27 10:59:30 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s -singly linked list
@@ -28,5 +29,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
