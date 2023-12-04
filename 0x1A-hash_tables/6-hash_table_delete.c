532132020e35ee587fed31cca8828ef442280490
patrickmuchiri <pmuchirig@gmail.com>
2023-12-04 08:40:08 -0800

    a function that deletes a hash table.

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a fuction that deletes a hash table.
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	if (ht == NULL)
+		return;
+
+	for (unsigned long int i = 0; i < ht->size; i++)
+	{
+		hash_node_t *current = ht->array[i];
+
+		while (current != NULL)
+		{
+			hash_node_t *next = current->next;
+
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
